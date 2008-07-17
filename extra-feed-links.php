5b132b163007acd1904b8241382444701ae8019d
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-07-17 16:27:38 +0000

    Adding version 0.6
    git-svn-id: https://plugins.svn.wordpress.org/extra-feed-links/trunk@55241 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- extra-feed-links.php
+++ extra-feed-links.php
@@ -1,111 +1,128 @@
 <?php
 /*
 Plugin Name: Extra Feed Links
-Version: 0.5
+Version: 0.6
 Description: Adds appropriate feed links to the header of posts, pages, categories, tags, search and author pages.
 Author: scribu
 Author URI: http://scribu.net/
-Plugin URI: http://scribu.net/downloads/extra-feed-links.html
+Plugin URI: http://scribu.net/download/extra-feed-links.html
 */
 
-/*
-Copyright (C) 2008 scribu.net (scribu AT gmail DOT com)
+class extraFeedLink {
+	var $title;
+	var $url;
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-function extra_feed_links(){
-	#Translatable text
-	$before = array(
-		'comments' => 'Comments: ',
-		'categoty' => 'Category: ',
-		'tag' => 'Tag: ',
-		'search' => 'Search: ',
-		'author' => 'Author: '
-	);
-
-	#For adding the "All comments" feed to the home page
-	/*if( is_home() ) {
-		$url = get_bloginfo('comments_rss2_url');
-		$title = 'All Comments';
+	function __construct(){
+		add_action('wp_head', array(&$this, 'head_link'));
 	}
-	else*/
 
-	if( is_single() || is_page() ){
-		global $post;
-		if ( 'open' == $post->comment_status ){
-			$url = get_post_comments_feed_link($post->ID);
-			$title = $before['comments'] . $post->post_title;
+	function head_link(){
+		$this->generate();
+
+		if( !$this->url || !$this->title)
+			return;
+
+		echo "\n" . '<link rel="alternate" type="application/rss+xml" title="' . $this->title . '" href="' . $this->url . '" />' . "\n";
+	}
+
+	function theme_link($input){
+		$this->generate();
+
+		if( !$this->url )
+			return;
+
+		if ( substr($input, 0, 4) == 'http' )
+			echo '<a href="' . $this->url . '" title="' . $this->title . '"><img src="' . $input . '" alt="rss icon" /></a>';
+		elseif ( $input == '' )
+			echo '<a href="' . $this->url . '" title="' . $this->title . '">' . $input . '</a>';
+		else
+			echo '<a href="' . $this->url . '" title="' . $this->title . '">' . $this->title . '</a>';
+	}
+
+	function generate(){
+		$before = array(
+			'comments' => 'Comments: ',
+			'category' => 'Category: ',
+			'tag' => 'Tag: ',
+			'search' => 'Search: ',
+			'author' => 'Author: '
+		);
+
+		/*if( is_home() ) {
+			$this->url = get_bloginfo('comments_rss2_url');
+			$this->title = 'All Comments';
+		}
+		else*/
+		if( is_single() || is_page() ){
+			global $post;
+			if ( 'open' == $post->comment_status ){
+				$this->url = get_post_comments_feed_link($post->ID);
+				$this->title = $before['comments'] . $post->post_title;
+			}
+		}
+		elseif ( is_category() ){
+			global $wp_query;
+			$cat_obj = $wp_query->get_queried_object();
+			$this->url = get_category_feed_link($cat_obj->term_id);
+			$this->title = $before['category'] . $cat_obj->name;
+		}
+		elseif ( is_tag() ){
+			global $wp_query;
+			$tag_obj = $wp_query->get_queried_object();
+			$this->url = $this->get_tag_feed_link($tag_obj->term_id);
+			$this->title = $before['tag'] . $tag_obj->name;
+		}
+		elseif ( is_search() ){
+			$search = attribute_escape(get_search_query());
+			$this->url = get_search_feed_link($search);
+			$this->title = $before['search'] . $search;
+		}
+		elseif ( is_author() ){
+			global $wp_query;
+			$author_obj = $wp_query->get_queried_object();
+			$this->url = get_author_feed_link($author_obj->ID);
+			$this->title = $before['author'] . $author_obj->user_nicename;
 		}
 	}
-	elseif ( is_category() ){
-		global $wp_query;
-		$cat_obj = $wp_query->get_queried_object();
-		$url = get_category_feed_link($cat_obj->term_id);
-		$title = $before['category'] . $cat_obj->name;
-	}
-	elseif ( is_tag() ){
-		global $wp_query;
-		$tag_obj = $wp_query->get_queried_object();
-		$url = get_fixed_tag_feed_link($tag_obj->term_id);
-		$title = $before['tag'] . $tag_obj->name;
-	}
-	elseif ( is_search() ){
-		$search = attribute_escape(get_search_query());
-		$url = get_search_feed_link($search);
-		$title = $before['search'] . $search;
-	}
-	elseif ( is_author() ){
-		global $wp_query;
-		$author_obj = $wp_query->get_queried_object();
-		$url = get_author_feed_link($author_obj->ID);
-		$title = $before['author'] . $author_obj->user_nicename;
-	}
 
-	if($url && $title)
-		echo "\n" . '<link rel="alternate" type="application/rss+xml" title="' . $title . '" href="' . $url . '" />' . "\n";
+	function get_tag_feed_link($tag_id, $feed = '') {
+		//for WP lower than 2.5.2
+		$tag_id = (int) $tag_id;
+
+		$tag = get_tag($tag_id);
+
+		if ( empty($tag) || is_wp_error($tag) )
+			return false;
+
+		$permalink_structure = get_option('permalink_structure');
+
+		if ( empty($feed) )
+			$feed = get_default_feed();
+
+		if ( '' == $permalink_structure ) {
+			$link = get_option('home') . "?feed=$feed&amp;tag=" . $tag->slug;
+		} else {
+			$link = get_tag_link($tag->term_id);
+			if ( $feed == get_default_feed() )
+				$feed_link = 'feed';
+			else
+				$feed_link = "feed/$feed";
+			$link = trailingslashit($link) . user_trailingslashit($feed_link, 'feed');
+		}
+
+		$link = apply_filters('tag_feed_link', $link, $feed);
+
+		return $link;
+	}
 }
 
-function get_fixed_tag_feed_link($tag_id, $feed = '') {
-	#for WP lower than 2.5.2
-	$tag_id = (int) $tag_id;
+global $extraFeedLink;
+$extraFeedLink = new extraFeedLink();
 
-	$tag = get_tag($tag_id);
+function extra_feed_link($input = ''){
+	global $extraFeedLink;
 
-	if ( empty($tag) || is_wp_error($tag) )
-		return false;
-
-	$permalink_structure = get_option('permalink_structure');
-
-	if ( empty($feed) )
-		$feed = get_default_feed();
-
-	if ( '' == $permalink_structure ) {
-		$link = get_option('home') . "?feed=$feed&amp;tag=" . $tag->slug;
-	} else {
-		$link = get_tag_link($tag->term_id);
-		if ( $feed == get_default_feed() )
-			$feed_link = 'feed';
-		else
-			$feed_link = "feed/$feed";
-		$link = trailingslashit($link) . user_trailingslashit($feed_link, 'feed');
-	}
-
-	$link = apply_filters('tag_feed_link', $link, $feed);
-
-	return $link;
+	$extraFeedLink->theme_link($input);
 }
 
-add_action('wp_head', 'extra_feed_links');
 ?>
